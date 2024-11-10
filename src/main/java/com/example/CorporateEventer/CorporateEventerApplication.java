f50af464a26d48ed8184db9dc746f0cdeba240af
Ivan_Karagachev <ivan.karagachev@gmail.com>
2024-11-10 14:42:59 +0300

    0.1

---
+++ src/main/java/com/example/CorporateEventer/CorporateEventerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CorporateEventer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateEventerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateEventerApplication.class, args);
+	}
+
+}
